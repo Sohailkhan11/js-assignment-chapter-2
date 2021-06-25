d69ea7ef188005f003b45ccbbadeaaa4754e37e3
Sohail Ahmed <sohailalijamali3@gmail.com>
2021-06-25 10:34:56 -0700

    2nd chapter of js

---
+++ aap.js
@@ -0,0 +1,17 @@
+var userage
+var myname
+var certified
+var pizza
+var email
+var book
+var a
+var line
+var content
+myname=alert("Sohail Ahmed")
+userage=alert("21 years old")
+certified-alert("certified Mobile Application Development")
+pizza=alert("pizza" + "\n" + "pizz"  + "\n" + "piz" + "\n" + "pi" + "\n" + "p")
+email=alert("My email address is example @ gmail.com")
+book=a=alert("I am trying To Learn from he book a smarter way to learn javascript")
+line=alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+content=document.write("yah! i can write html content through js")
